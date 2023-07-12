89b220a8e4e25d57e02ddfadbaff112fa643e2cf
Pinerealm <osalami09@gmail.com>
2023-07-12 11:14:16 +0300

    Check that a square should have equal sides

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,28 +1,35 @@
 #!/usr/bin/python3
 
-class square():
-    
+class Square():
+    """ Square class """
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+        """ Initialize a new square object """
+        if kwargs:
+            if "width" in kwargs:
+                self.width = kwargs["width"]
+            if "height" in kwargs:
+                if kwargs["height"] == self.width:
+                    self.height = kwargs["height"]
+                else:
+                    self.height = self.width = 0
 
     def area_of_my_square(self):
         """ Area of the square """
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ Perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
 
-if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+if __name__ == "__main__":
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
