6137cfc96d599c831db6d545793c841301ac2c30
Pinerealm <osalami09@gmail.com>
2026-07-04 08:32:11 +0300

    Refactor Square class documentation and method names

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Square module."""
 
 class Square():
     """ Square class """
@@ -6,7 +7,16 @@ class Square():
     height = 0
 
     def __init__(self, *args, **kwargs):
-        """ Initialize a new square object """
+        """Initialize a new Square instance.
+
+        Accepts width and height as keyword arguments. If both are provided
+        the instance will keep them only if they are equal (a square). If
+        the provided height differs from the width, both attributes are
+        set to 0 to indicate an invalid square.
+
+        Example:
+            Square(width=5, height=5)
+        """
         if kwargs:
             if "width" in kwargs:
                 self.width = kwargs["width"]
@@ -17,14 +27,25 @@ class Square():
                     self.height = self.width = 0
 
     def area_of_my_square(self):
-        """ Area of the square """
+        """Return the area of the square (width * height).
+
+        Returns 0 if the square is invalid (width or height is 0).
+        """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
-        """ Perimeter of the square """
+    def permiter_of_my_square(self):
+        """Return the perimeter of the square (2*width + 2*height).
+
+        Returns 0 if the square is invalid (width or height is 0).
+        Note: method name intentionally matches original code.
+        """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """Return a short string representation of the square.
+
+        Format: "width/height" (e.g. "5/5").
+        """
         return "{}/{}".format(self.width, self.height)
 
 
@@ -32,4 +53,4 @@ if __name__ == "__main__":
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
